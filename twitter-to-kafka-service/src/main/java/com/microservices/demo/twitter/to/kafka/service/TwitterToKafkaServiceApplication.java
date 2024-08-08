05654da61cf18085c12429485c2c117328181f99
saksham-sarkar <saksham.sarkar2791@gmail.com>
2024-08-08 10:36:45 +0530

    Initial commit

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,47 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private static Logger LOG = LoggerFactory.getLogger("TwitterToKafkaServiceApplication.class");
+
+    private  final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
+        this.twitterToKafkaServiceConfigData = configData;
+
+    }
+    public static void main(String[] args) {
+        LOG.info("SpringApplication before starts...");
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+        LOG.info("SpringApplication after starts...");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App starts...");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
